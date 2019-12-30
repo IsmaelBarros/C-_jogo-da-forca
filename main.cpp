b8fa7e4af707aa970c6286f6f5060e881d5a0411
Ismael Barros <ismasbarros2@gmail.com>
2019-12-30 12:58:25 -0300

    added files

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char palavra[30], letra[1], secreta[30];
+    int tam,i,chances, acertos;
+    bool acerto;
+
+    i = 0;
+    chances = 6;
+    tam = 0;
+    acerto = false;
+    acertos = 0;
+
+
+    cout<< "JOGO DA FORCA\n\n";
+    cout<< "Digite a palavra secreta \n\n";
+    cin>> palavra;
+
+    //cout<< palavra;
+
+    return 0;
+}
